1e198f9deb9e50604be773257f73c9eaadba2c74
weekkkk <93500964+weekkkk@users.noreply.github.com>
2021-11-02 15:01:10 +0200

    Task
    TaskInternship

---
+++ script.js
@@ -0,0 +1,8 @@
+function navOpen() {
+    document.getElementById('navButtonOpen').style.display = 'none'
+    document.getElementById('navBar').style.display = 'inline-block'
+}
+function navClose() {
+    document.getElementById('navBar').style.display = 'none'
+    document.getElementById('navButtonOpen').style.display = 'inline-block'
+}
